7c061b262c8a1f0edfa9ca68a0484e89b2b2ef80
Mayur Patel <pmayur0680@gmail.com>
2022-04-19 18:44:20 -0500

    updated portfolio with latest projects

---
+++ assets/js/script.js
@@ -0,0 +1,20 @@
+var projectStockSimulatorEl = document.querySelector(".project-stock-simulator");
+var projectWeatherDashboardEl = document.querySelector(".project-weather-dashboard");
+var projectWorkDaySchedulerEl = document.querySelector(".project-workday-scheduler");
+
+// Project - Stock Simulator link
+projectStockSimulatorEl.addEventListener("click", function() {
+    window.open('https://github.com/yummy314159265/StockMarketSimulator');
+});
+
+// Project - Weather Dashboard link
+
+projectWeatherDashboardEl.addEventListener("click", function() {
+    window.open('https://github.com/pmayur0680/Weather-Dashboard');
+});
+
+// Project - Work Day Scheduler link
+
+projectWorkDaySchedulerEl.addEventListener("click", function() {
+    window.open('https://github.com/pmayur0680/Work-Day-Scheduler');
+});
